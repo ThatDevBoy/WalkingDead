df8cf635a151189a99ee009c58fce23660b04339
Laxer2117 <seth419@gmail.com>
2014-12-26 23:00:54 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.DevBoy.Infect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	
+	public void onEnable() {
+		System.out.println("TheInfection is now enabled!");
+	}
+	
+	public void onDisable() {
+		System.out.println("TheInfection is now disabled!");
+	}
+	
+	
+
+}
