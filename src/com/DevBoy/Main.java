ce0fcdac16d768b730e50990cb128a484eba2945
Laxer2117 <seth419@gmail.com>
2015-02-16 09:27:18 -0500

    Update Main.java

--- src/com/DevBoy/Main.java
+++ src/com/DevBoy/Main.java
@@ -3,27 +3,35 @@ package com.DevBoy.Infect;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	
 	// Main class is still a WIP
 	
 	public void onEnable() {
 		System.out.println("TheInfection is now enabled!");
-		SettingsManager.getInstance().setup(this);
+			
+		}
 		
-	}
-	
 	public void onDisable() {
 		System.out.println("TheInfection is now disabled!");
-	}
-	
+		}
+		
 	public boolean onCommand(CommandSender p, Command cmd, String label, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("infect")) {
 			if(p.hasPermission("infect.help")) {
-				sender.sendMessage(ChatColor.RED + "   -= Infect Help Menu -=    ");
+				p.sendMessage(ChatColor.RED + "   -= Infect Help Menu -=    ");
 			}else {
-				p.sendMessage(ChatColor.RED + "You don't have the required permission! You must have the permission infect.help!);
+				p.sendMessage(ChatColor.RED + "You don't have the required permission! You must have the permission infect.help!");
 			}
-	}
-	
+		}
+			return false;
 
 }
-
+}
