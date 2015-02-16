62597f2d9446bacfac12203e8ce43d1e0b38bb4a
Laxer2117 <seth419@gmail.com>
2015-02-16 09:22:25 -0500

    Update Main.java

--- src/com/DevBoy/Main.java
+++ src/com/DevBoy/Main.java
@@ -13,15 +13,20 @@ public class Main extends JavaPlugin {
 		System.out.println("TheInfection is now enabled!");
 		SettingsManager.getInstance().setup(this);
 		
-		SettingsManager().getInstance().set("sampleteam", new Squads("SquadDev").serialize());
-		Squads squad = new Squads (SettingsManager.getInstance().<Map<String, Object>> get("sampleteam");
-		System.out.println(Squads.getName());
 	}
 	
 	public void onDisable() {
 		System.out.println("TheInfection is now disabled!");
 	}
 	
+	public boolean onCommand(CommandSender p, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("infect")) {
+			if(p.hasPermission("infect.help")) {
+				sender.sendMessage(ChatColor.RED + "   -= Infect Help Menu -=    ");
+			}else {
+				p.sendMessage(ChatColor.RED + "You don't have the required permission! You must have the permission infect.help!);
+			}
+	}
 	
 
 }
